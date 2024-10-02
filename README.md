e93c7b11fd2489dfc2a3d51cda2159fee6f93c82
Felipe Lazzarini <felipe.mumei@gmail.com>
2024-10-01 23:26:34 -0300

    Variáveis globais

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
+# Notas sobre o Programa
+- Para fins de simplicidade, adotei valores estáticos ao invés de alocar dinâmicamente em tempo de execução
+
 # Notas sobre o Fortran
 - Boa prática declarar no início do programa ```implicit none```, para indicar para o compilador que todas as variáveis vão ser explicitamente declaradas
 - Podemos alocar dinâmicamente as strings, como em C, mas para simplicidade do projeto vou manter valores fixos de 20 e 160, para nome e descrição dos produtos
 - $ gfortran programa.f95 + $./a.out -> comando para rodar sem gerar outros arquivos adicionais, como é o comportamento padrão (no Linux)
 - gcc-fortran -> pacote no Arch para rodar Fortran
+- function funciona como função
+- modulos podem ser usados para declarar variaveis globais
+- subrotina implementada para atuar como void
--- listagem.f95
+++ listagem.f95
@@ -1,30 +1,49 @@
 ! Projeto em: https://github.com/Muowl/prova_cadastro_fortran
-function cadastro(id) result(j)
-    implicit none 
+module dados_globais
+    implicit none
     !nome do produto = string
     !descrição do produto = string
-    character(len = 20) :: nome
-    character(len = 160) :: descricao
-    real :: valor
-    logical :: disponivel
+    character(len = 20) :: nomes(5)
+    character(len = 160) :: descricoes(5)
+    real :: valores(5)
+    logical :: disponivel(5)
+    integer :: id_produto(5)
 
+end module dados_globais
+
+function cadastro(id)
+    implicit none 
     print *, 'Digite o nome do produto: '
-    read *, nome
+    read *, nome(id)
 
     print *, 'Digite a descricao do produto: '
-    read *, descricao
+    read *, descricao(id)
 
     print *, 'Digite o valor do produto: '
-    read *, valor
+    read *, valor(id)
 
-    print *, 'Valores digitados: nome: ', nome, ' descricao: ', descricao, 'valor: '
-    print '(F6.2)', valor
+    print *, 'Digite se está disponível: '
+    read *, valor(id)
 
+    !print *, 'Valores digitados: nome: ', nome, ' descricao: ', descricao, 'valor: '
+    !print '(F6.2)', valor
 end function
 
+subroutine exibir()
+    implicit none
+
+    print *, 'Esta subrotina não retorna nenhum valor.'
+
+end subroutine exibir
 
 program listagem
     implicit none 
+    integer :: parada
+    parada = 10
 
+    do while (parada == 10)
+        id_produto += id_produto
+        cadastro(id_produto)
+        call exibir()
 
 end program cadastro
