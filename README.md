5e0c73c7b246974ff8acb4cfdaad140a34561757
Felipe Lazzarini <felipe.mumei@gmail.com>
2024-10-01 22:33:42 -0300

    Leitura e escrita no terminal

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Notas sobre o Fortran
 - Boa prática declarar no início do programa ```implicit none```, para indicar para o compilador que todas as variáveis vão ser explicitamente declaradas
+- Podemos alocar dinâmicamente as strings, como em C, mas para simplicidade do projeto vou manter valores fixos de 20 e 160, para nome e descrição dos produtos
--- cadastro.f95
+++ cadastro.f95
@@ -3,7 +3,20 @@ program cadastro
     implicit none 
     !nome do produto = string
     !descrição do produto = string
+    character(len = 20) :: nome
+    character(len = 160) :: descricao
     real :: valor
     logical :: disponivel
+
+    print *, 'Digite seu primeiro nome: '
+    read *, nome
+
+    print *, 'Digite seu segundo nome: '
+    read *, descricao
+
+    print *, 'Digite o valor do produto: '
+    read *, valor
+
+    print *, 'Valores digitados: ', nome, ' ', descricao, ' ', valor
 
 end program cadastro
