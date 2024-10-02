e1d72e4c46537878e0b55bdaa239c491176df4a6
Felipe Lazzarini <felipe.mumei@gmail.com>
2024-10-02 00:50:51 -0300

    Versão inicial e ajuste lógico

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
+# Rodar o Projeto
+- Ter compilador Fortran instalado 
+    - Pacote gcc-fortran no Arch
+    - Pacote gfortran no Ubuntu
+    - Para Windows: https://www.youtube.com/watch?v=RrsoM6wVEWE
+- Na pasta do projeto rodar o comando ```gfortran listagem.f95 && ./a.out``` (Linux) ou ```gfortran listagem.f95 && ./a.exe``` (Windows)
+
 # Notas sobre o Programa
 - Para fins de simplicidade, adotei valores estáticos ao invés de alocar dinâmicamente em tempo de execução
 
-# Notas sobre o Fortran
+# Planos de Implementação
+- Melhoria na exibição do console
+- GUI (a ver gtk-fortran, winteracter)
+- Persistência de dados (armazenando dados em um arquivo bin ou dat)
+
+# Notas Sobre o Fortran
 - Boa prática declarar no início do programa ```implicit none```, para indicar para o compilador que todas as variáveis vão ser explicitamente declaradas
 - Podemos alocar dinâmicamente as strings, como em C, mas para simplicidade do projeto vou manter valores fixos de 20 e 160, para nome e descrição dos produtos
-- $ gfortran programa.f95 + $./a.out -> comando para rodar sem gerar outros arquivos adicionais, como é o comportamento padrão (no Linux)
+- $ gfortran programa.f95 && ./a.out -> comando para rodar sem gerar outros arquivos adicionais, como é o comportamento padrão (no Linux)
 - gcc-fortran -> pacote no Arch para rodar Fortran
 - function funciona como função
 - modulos podem ser usados para declarar variaveis globais
 - subrotina implementada para atuar como void
+- troca de function cadastro para subrotina pelo motivo acima
+- / é usada como sinal de diferente !=
+- write (*,*) uma alternativa para exibir na tela controlando quebras de linhas e com possíveis formatações
--- listagem.f95
+++ listagem.f95
@@ -1,4 +1,6 @@
+
 ! Projeto em: https://github.com/Muowl/prova_cadastro_fortran
+
 module dados_globais
     implicit none
     !nome do produto = string
@@ -6,44 +8,58 @@ module dados_globais
     character(len = 20) :: nomes(5)
     character(len = 160) :: descricoes(5)
     real :: valores(5)
-    logical :: disponivel(5)
-    integer :: id_produto(5)
+    integer :: disponivel(5)
+    integer :: id_produto = 0
 
 end module dados_globais
 
-function cadastro(id)
+subroutine cadastro(id)
+    use dados_globais
     implicit none 
+    integer, intent(in) :: id  !declarar o valor de entrada
+
     print *, 'Digite o nome do produto: '
-    read *, nome(id)
+    read *, nomes(id)
 
     print *, 'Digite a descricao do produto: '
-    read *, descricao(id)
+    read *, descricoes(id)
 
     print *, 'Digite o valor do produto: '
-    read *, valor(id)
+    read *, valores(id)
 
-    print *, 'Digite se está disponível: '
-    read *, valor(id)
+    print *, 'Digite quantidade disponível: '
+    read *, disponivel(id)
 
     !print *, 'Valores digitados: nome: ', nome, ' descricao: ', descricao, 'valor: '
     !print '(F6.2)', valor
-end function
+end subroutine cadastro
 
 subroutine exibir()
+    use dados_globais
     implicit none
+    integer :: i
 
-    print *, 'Esta subrotina não retorna nenhum valor.'
+    do i = 1, id_produto
+        print *, '-------------------------------------------'        
+        write(*,*) 'ID: ', i, ' | Nome: ', nomes(i), ' | Descricao: ', descricoes(i), ' | Valores: ', valores(i), ' | Disponivel: ', disponivel(i)
+        print *, '-------------------------------------------'
+    end do
 
 end subroutine exibir
 
 program listagem
+    use dados_globais
     implicit none 
     integer :: parada
-    parada = 10
+    parada = 1
 
-    do while (parada == 10)
-        id_produto += id_produto
-        cadastro(id_produto)
+    do while (parada == 1 .and. id_produto < 5)
+        id_produto = id_produto + 1
+        call cadastro(id_produto)
         call exibir()
+        print *, 'Digite 1 para cadastrar outro produto ou qualquer outro numero para terminar o programa: '
+        read *, parada
+    end do
 
-end program cadastro
+    print *, 'Fim do programa'
+end program listagem
