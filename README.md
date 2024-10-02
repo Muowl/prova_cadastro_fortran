3705e043837eae3582726c71fe0eb90b2f1fbc95
Felipe Lazzarini <felipe.mumei@gmail.com>
2024-10-01 22:12:16 -0300

    Estrutura inicial do projeto

---
+++ README.md
@@ -0,0 +1,2 @@
+# Notas sobre o Fortran
+- Boa prática declarar no início do programa ```implicit none```, para indicar para o compilador que todas as variáveis vão ser explicitamente declaradas
---
+++ cadastro.f95
@@ -0,0 +1,9 @@
+! Projeto em: https://github.com/Muowl/prova_cadastro_fortran
+program cadastro
+    implicit none 
+    !nome do produto = string
+    !descrição do produto = string
+    real :: valor
+    logical :: disponivel
+
+end program cadastro
